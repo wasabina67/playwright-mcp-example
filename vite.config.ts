ac86e48c80038b2ee68a5d338be1da2d057edf8b
wasabina67 <wasabina67@gmail.com>
2025-04-21 22:05:21 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/playwright-mcp-example/',
+  build: {
+    outDir: 'docs',
+  },
 })
