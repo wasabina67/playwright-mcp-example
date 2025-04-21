76bf2bf75ee7cfe17b6cefc022d59ca68bfb96e8
wasabina67 <wasabina67@gmail.com>
2025-04-21 22:44:15 +0900

    Update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [likes, setLikes] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="#" target="_blank">
+          <img src="/assets/logo-1.svg" className="logo" alt="" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="#" target="_blank">
+          <img src="/assets/logo-2.svg" className="logo react" alt="" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Welcome to Our Pasta House</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setLikes((likes) => likes + 1)}>
+          Likes: {likes}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        Discover our delicious pasta recipes and visit us for an unforgettable dining experience.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        We look forward to your visit.
       </p>
     </>
   )
