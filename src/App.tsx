67f2770d92aa5bca61bb474c79fa9f565cf3fa69
wasabina67 <wasabina67@gmail.com>
2025-04-29 17:37:12 +0900

    Update src/App.tsx & Create public/logo.png

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ function App() {
     <>
       <div>
         <a href="#">
-          <img src="vite.svg" className="logo" alt="Logo 1" />
+          <img src="logo.png" className="logo" alt="Logo 1" />
         </a>
         <a href="#">
-          <img src="vite.svg" className="logo react" alt="Logo 2" />
+          <img src="logo.png" className="logo react" alt="Logo 2" />
         </a>
       </div>
       <h1>ザ・アルデンテ</h1>
       <div className="card">
         <p>
-          1977年に新宿で創業したスパゲッティ専門店。
+          1993年に新宿で創業したスパゲッティ専門店。
         </p>
         <p>
           絶妙なアルデンテの食感とコシの強い麺が特徴です。🍝🍝🍝
